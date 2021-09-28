9e37a2df2d369486a30c4f14ff0ca2dd158b96f1
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-28 23:42:00 +0530

    Added lombok for logging

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeData.java
@@ -2,7 +2,9 @@ package com.bridgelabz.employeepayrollapp.model;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 
-public class EmployeeData {
+import lombok.Data;
+
+public @Data class EmployeeData {
 	
 	private Integer emp_id;
 	private String name;
@@ -12,24 +14,5 @@ public class EmployeeData {
 		this.emp_id = emp_id;
 		this.name= dataDTO.getName();
 		this.salary = dataDTO.getSalary();
-	}
-	public Integer getEmpID() {
-		return emp_id;
-	}
-	public void setEmpID(Integer emp_id) {
-		this.emp_id = emp_id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getSalary() {
-		return salary;
-	}
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-	
+	}	
 }
